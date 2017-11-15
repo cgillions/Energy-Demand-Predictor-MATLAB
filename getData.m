c068d69eb0e6338727ee6356cacadbf36f829200
Charlie <charlie@Charlies-MacBook-Pro.local>
2017-11-15 15:26:32 +0000

    Removed anomalies from the datasets in order to reduce training the network on unusual data - skewing our prediction results.

--- getData.m
+++ getData.m
@@ -3,4 +3,18 @@ function [x, target] = getData(filename)
     data   = load(filename);
     x      = data(:, 1:3);
     target = data(:, 4);
+    
+    % Remove anomalies from the data set.
+    % https://uk.mathworks.com/matlabcentral/answers/298250-how-do-i-remove-outliers-from-a-matrix
+    disp("Initial size is: " + size(x, 1));
+    
+    % Find the row ids of outlying data points.
+    idx = bsxfun(@gt, target, mean(target) + 1.66 * std(target)) | bsxfun(@lt, target, mean(target) - 1.66 * std(target));
+    idx = any(idx, 2);
+    
+    % Remove those rows from the data set.
+    target(idx, :) = [];
+    x(idx, :)      = [];
+    
+    disp("Final size is: " + size(x, 1));
 end
